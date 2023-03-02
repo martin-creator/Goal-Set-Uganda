36b0d4da600732fd68bc04353b51863cee09b9f5
martin-creator <martinlubowa@outlook.com>
2023-03-02 16:07:53 +0300

    Renamed dashbord to student dasboard

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -13,7 +13,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('Student Dashboard') }}
                     </x-jet-nav-link>
                     @if ( Route::currentRouteName()!= 'admin.dashboard')
                     <x-jet-nav-link href="{{ route('dashboard.academics') }}" :active="request()->routeIs('dashboard.academics')">
