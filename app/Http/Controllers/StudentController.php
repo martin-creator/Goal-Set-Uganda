66cdcbfb5824a6ae406744a99aa166823a6b81d4
martin-creator <martinlubowa@outlook.com>
2023-03-03 23:08:17 +0300

    Added models to users controller

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Person;
+use App\Models\Education;
+use App\Models\Entrepreneurship;
+use Illuminate\Support\Facades\Auth;
+
 
 class StudentController extends Controller
 {
