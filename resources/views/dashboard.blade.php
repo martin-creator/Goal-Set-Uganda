a6d26a8c8a3bca6f61db9a408604f738ecc26926
martin-creator <martinlubowa@outlook.com>
2023-03-05 21:04:46 +0300

    Added tables

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,19 +3,15 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
+
     </x-slot>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 ">
 
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 @livewire('set-targets')
-                <x-jet-welcome />
-                @livewire('set-targets') -
             </div>
         </div>
-
-
-
     </div>
 </x-app-layout>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const twElements = require('tw-elements');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
