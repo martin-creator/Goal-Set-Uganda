7447e660e26af9f2b65cfeb47ef94231d61b4c77
martin-creator <martinlubowa@outlook.com>
2023-03-05 08:11:44 +0300

    Added inpiration quotes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,17 +4,19 @@
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
-
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                @if(Route::currentRouteName()== 'dashboard.academics')
-                    <x-jet-welcome />
-                @else:
-                    <x-jet-welcome />
-                @endif
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 ">
 
-            </div>
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                @livewire('set-targets')
+                {{-- <x-jet-welcome />
+                @livewire('set-targets') --}}
+            {{-- </div>
         </div>
+
+
+
     </div>
 </x-app-layout>
+
+
