9df72e8c522ac1d518a89d56e951cfe71fc328f0
martin-creator <martinlubowa@outlook.com>
2023-03-06 09:46:58 +0300

    Fixed UI&UX on dashboard

--- resources/views/livewire/set-targets.blade.php
+++ resources/views/livewire/set-targets.blade.php
@@ -21,6 +21,9 @@
         simple steps, you will be able to achieve anything you set your mind to.
     </div>
 </div>
+<div class="mt-10 text-2xl text-center">
+    Goal Setting Summary
+</div>
 
 <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
     <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
@@ -36,7 +39,7 @@
                 <th scope="col" class="px-6 py-4">DIV</th>
               </tr>
             </thead>
-            <tbody>
+            <tbody class="text-sm">
               <tr class="border-b dark:border-neutral-500 hover:bg-gray-100">
                 <td class="whitespace-nowrap px-6 py-4 font-medium">Maths</td>
                 <td class="whitespace-nowrap px-6 py-4">1</td>
@@ -45,14 +48,35 @@
                 <td class="whitespace-nowrap px-6 py-4">2</td>
               </tr>
               <tr class="border-b dark:border-neutral-500 hover:bg-gray-100">
-                <td class="whitespace-nowrap px-6 py-4 font-medium">Biology</td>
+                <td class="whitespace-nowrap px-6 py-4 font-medium">Bio</td>
                 <td class="whitespace-nowrap px-6 py-4">2</td>
                 <td class="whitespace-nowrap px-6 py-4">70</td>
                 <td class="whitespace-nowrap px-6 py-4">50</td>
                 <td class="whitespace-nowrap px-6 py-4">2</td>
               </tr>
               <tr class="border-b dark:border-neutral-500 hover:bg-gray-100 ">
-                <td class="whitespace-nowrap px-6 py-4 font-medium">Chemistry</td>
+                <td class="whitespace-nowrap px-6 py-4 font-medium">Chem.</td>
+                <td class="whitespace-nowrap px-6 py-4">3</td>
+                <td class="whitespace-nowrap px-6 py-4">86</td>
+                <td class="whitespace-nowrap px-6 py-4">50</td>
+                <td class="whitespace-nowrap px-6 py-4">2</td>
+              </tr>
+              <tr class="border-b dark:border-neutral-500 hover:bg-gray-100 ">
+                <td class="whitespace-nowrap px-6 py-4 font-medium">Geo.</td>
+                <td class="whitespace-nowrap px-6 py-4">3</td>
+                <td class="whitespace-nowrap px-6 py-4">86</td>
+                <td class="whitespace-nowrap px-6 py-4">50</td>
+                <td class="whitespace-nowrap px-6 py-4">2</td>
+              </tr>
+              <tr class="border-b dark:border-neutral-500 hover:bg-gray-100 ">
+                <td class="whitespace-nowrap px-6 py-4 font-medium">His.</td>
+                <td class="whitespace-nowrap px-6 py-4">3</td>
+                <td class="whitespace-nowrap px-6 py-4">86</td>
+                <td class="whitespace-nowrap px-6 py-4">50</td>
+                <td class="whitespace-nowrap px-6 py-4">2</td>
+              </tr>
+              <tr class="border-b dark:border-neutral-500 hover:bg-gray-100 ">
+                <td class="whitespace-nowrap px-6 py-4 font-medium">Lit.</td>
                 <td class="whitespace-nowrap px-6 py-4">3</td>
                 <td class="whitespace-nowrap px-6 py-4">86</td>
                 <td class="whitespace-nowrap px-6 py-4">50</td>
@@ -60,14 +84,17 @@
               </tr>
             </tbody>
           </table>
+          <p class="mt-8 text-sm text-center"> <strong>Abbreviations: TM = Target Marks, AM = Actual Marks, DIV = Deviation</strong></p>
         </div>
       </div>
     </div>
   </div>
 
+  <div class="mt-10 text-2xl text-center mb-6">
+    Reading Resources
+</div>
 
-
-<div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2">
+<div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2 mt-4">
     <div class="p-6">
         <div class="flex items-center">
             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-400"><path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg>
@@ -146,12 +173,12 @@
     <div class="p-6 border-t border-gray-200 dark:border-gray-700 mt-5 ">
         <div class="flex items-center">
             <div class="mx-auto text-lg leading-7 font-semibold text-center"><a
-                    class="underline text-gray-900 dark:text-white text-center">Quotes of the Day</a></div>
+                    class="underline text-gray-900 dark:text-white text-center">Support</a></div>
         </div>
 
         <div class="ml-12">
             <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm text-center">
-                <h3>For guidance call, Wamala <a href="+256752400627">+256752400627</a> or email <a href="wamalahen@gmail.com">wamalahen@gmail.com</a>
+                <h3>For guidance call, Wamala Henry <a href="tel:+256752400627">+256752400627</a> or email <a href="mailto:wamalahen@gmail.com">wamalahen@gmail.com</a>
                 </h3>
             </div>
         </div>
