e1fba30b52942a75ab56af26f6099de8c73cad40
martin-creator <martinlubowa@outlook.com>
2023-03-06 10:36:38 +0300

    Created  subject one livewire component

---
+++ app/Http/Livewire/CompulsorySubjectOne.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class CompulsorySubjectOne extends Component
+{
+    public function render()
+    {
+        return view('livewire.compulsory-subject-one');
+    }
+}
